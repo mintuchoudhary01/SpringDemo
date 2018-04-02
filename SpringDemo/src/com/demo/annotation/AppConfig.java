57ce0ac656413932d423d788cccef3b1756e7ad1
Mintu Choudhary <mintu_choudhary01@yahoo.in>
2018-04-02 19:12:57 +0530

    Add files via upload

--- SpringDemo/src/com/demo/annotation/AppConfig.java
+++ SpringDemo/src/com/demo/annotation/AppConfig.java
@@ -1,9 +1,11 @@
 package com.demo.annotation;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan("com.demo.annotation")
 public class AppConfig {
 
     @Bean(name="custBean")   
--- SpringDemo/src/com/demo/annotation/CustImpl.java
+++ SpringDemo/src/com/demo/annotation/CustImpl.java
@@ -2,15 +2,15 @@ package com.demo.annotation;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
-public class CustImpl implements Cust{
-//	@Autowired
-//	Address service;
+public class CustImpl implements Cust {
+	@Autowired
+	Address service;
 
 	@Override
 	public void printCust(String name) {
 		System.out.println("Hello : " + name);
 
-		//service.printAddressLine("Pune, Maharashtra");
+		service.printAddressLine("Pune: passed from CustImpl to Address.printAddressLine(..) ");
 	}
 
 }
